fc273c88fe94b9b6a99b58ceaf82aca24784d31b
shabn <shabnajva@gmail.com>
2025-04-21 10:52:05 +0330

    Add project files.

---
+++ SqlCoder/Controllers/TextToSqlController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+   [ApiController]
+   [Route("api/[controller]")]
+   public class TextToSqlController : Controller
+   {
+      private readonly OllamaService _ollamaService;
+
+
+      public TextToSqlController(OllamaService ollamaService)
+      {
+         _ollamaService = ollamaService;
+      }
+
+      [HttpPost]
+      public async Task<IActionResult> Post([FromBody] PromptInput input)
+      {
+
+         var prompt=input.Prompt;
+         var sql = await _ollamaService.GenerateSqlAsync(prompt);
+         return Ok(new { sql });
+      }
+   }
+   // Models/PromptInput.cs
+   public class PromptInput
+   {
+      public string Prompt { get; set; }
+   }
+}
+
---
+++ SqlCoder/Service/OllamaService.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Text;
+
+namespace WebApplication1.Services
+{
+   public class OllamaService
+   {
+      private readonly HttpClient _httpClient;
+
+      public OllamaService(HttpClient httpClient)
+      {
+         _httpClient = httpClient;
+      }
+
+      public async Task<string> GenerateSqlAsync(string prompt)
+      {
+         var requestBody = new
+         {
+            model = "sqlcoder",
+            prompt = prompt,
+            stream = false
+         };
+
+         var content = new StringContent(
+             JsonSerializer.Serialize(requestBody),
+             Encoding.UTF8,
+             "application/json");
+
+         var response = await _httpClient.PostAsync("http://localhost:11434/api/generate", content);
+         response.EnsureSuccessStatusCode();
+         var responseString = await response.Content.ReadAsStringAsync();
+         var json = JsonDocument.Parse(responseString);
+         return json.RootElement.GetProperty("response").GetString();
+      }
+   }
+}
